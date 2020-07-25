a1f556eff5e01bb1757183032c8c0fa748e5395e
Ichsan N <nugraha.ichsan99@gmail.com>
2020-07-25 22:30:30 +0700

    2

--- app/src/main/java/com/tubesmanpropel/devhouse/MainActivity.java
+++ app/src/main/java/com/tubesmanpropel/devhouse/MainActivity.java
@@ -1,9 +1,14 @@
 package com.tubesmanpropel.devhouse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setSelectedItemId(R.id.nav_home);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new UserHomeFragment()).commit();
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new UserHomeFragment();
+                            break;
+                        case R.id.nav_feed:
+                            selectedFragment = new UserFeedFragment();
+                            break;
+                        case R.id.nav_favorite:
+                            selectedFragment = new UserFavoriteFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new UserProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
